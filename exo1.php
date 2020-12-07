21cf406dc40fc7bf67bf5bd2c7e3d7667d4128ac
Alison-Denis <74008946+Alison-Denis@users.noreply.github.com>
2020-12-07 14:04:00 +0100

    Add files via upload

---
+++ exo1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 1 - Partie 1</title>
+</head>
+
+<body>
+
+<?php
+echo "Bonjour le monde";
+$name = "Ceci est une variable de mon choix";
+echo $name;
+?>
+
+</body>
+</html>
